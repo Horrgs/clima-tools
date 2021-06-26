5e92392403268be4d5934fd23f6fed5b9a525b7b
MJ <Horrgs@users.noreply.github.com>
2021-06-26 05:49:41 -0400

    Updates to GUI.

--- main.py
+++ main.py
@@ -1,192 +1,178 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
-from sort import build
-from pathlib import Path
-from search import Search
+from search import Search, SearchType
 
 
 class Home:
     def __init__(self):
 
+        # Initialize GUI interface, set title and resolution.
         root = Tk()
         root.title("Clima-tools")
         root.geometry("1200x600")
-        root.update()
-
         root.columnconfigure(0, weight=1)  # centers column
         root.rowconfigure(0, weight=1)  # centers rows
-        frame = ttk.Frame(root, name="main")
-        frame.grid(column=0, row=0)
-        self.advanced = False
+        root.update()
 
+        frame = ttk.Frame(root, name="main")  # add frame to GUI.
+        frame.grid(column=0, row=0)  # place frame in GUI grid.
+
+        # set program attributes.
+        self.advanced = False  # advanced settings is set to False.
+        self.choices = {}  # choices made in advanced settings
         self.root = root
         self.frame = frame
-        self.build_home_screen()
+        self.search = Search()
+
+        self.padding = self.root.winfo_width() * .005
+        self.build_home_screen()  # build the home screen, adding it to our first frame.
         self.root.mainloop()
 
-
     def build_home_screen(self):
-        single_station = Button(self.frame, text="Single Station File", command=self.search)
-        bulk_stations = Button(self.frame, text="Multiple Stations File", command=None)
-        padding = self.root.winfo_width()*.005
-        single_station.grid(column=0, row=0, padx=padding)
-        bulk_stations.grid(column=1, row=0, padx=padding)
+        self.destroy_widgets()
 
-    def search(self):
-        padding = self.root.winfo_width()*.005
-        for widget in self.frame.winfo_children():
-            widget.destroy()
+        local = Button(self.frame, text="Local File", command=self.local_file)
+        local.grid(column=0, row=0, sticky='ew')
 
+        api = Button(self.frame, text="NOAA Online Request", command=None)
+        api.grid(column=0, row=1, pady=self.padding, sticky='ew')
 
+    def fetch_file(self, event):
+        f_path = filedialog.askopenfilename()
+        event.widget.configure(state="normal")
+        event.widget.delete(0, END)
+        event.widget.insert(END, f_path)
+        event.widget.configure(state="readonly")
+        event.widget.xview_moveto(1)
+
+        self.search.update({'local_file': event.widget.get()})
+
+        self.choices = {
+            "monthly": BooleanVar(value=False),
+            "separate_files": BooleanVar(value=False),
+            "date": BooleanVar(value=False),
+            "output_options": {
+                "csv": BooleanVar(value=False),
+                "png": BooleanVar(value=False),
+            },
+            "header_options": {}
+        }
+
+        for header in self.search.input_headers:
+            self.choices['header_options'][header] = BooleanVar(value=False)
+
+    def local_file(self):
+        self.destroy_widgets()
+        self.search.update({'search_type': SearchType.LOCAL_FILE})
 
         file_label = Label(self.frame, text="File Path:")
-        file_entry = Entry(self.frame, name="file_entry", state="readonly")
+        t = StringVar(self.frame, value=self.search.local_file)
+        file_entry = Entry(self.frame, name="file_entry", state="readonly", textvariable=t)
+        load_file = Button(self.frame, text="Next", name="load_file", command=self.output_options)
+        back = Button(self.frame, text="Back", command=self.build_home_screen)
 
-        def fetch_file(event):
-            f_path = filedialog.askopenfilename()
-            file_entry.configure(state="normal")
-            file_entry.delete(0, END)
-            file_entry.insert(END, f_path)
-            file_entry.configure(state="readonly")
-            file_entry.xview_moveto(1)
+        file_entry.bind('<1>', self.fetch_file)
 
-        advanced = Button(self.frame, text="Advanced Settings", name="settings", command=lambda: self.settings(file_entry.get(), not self.advanced))
-        submit = Button(self.frame, text="Submit", name="submit", command=lambda: self.submit(file_entry.get()))
+        file_label.grid(column=0, row=0, padx=self.padding)
+        file_entry.grid(column=1, row=0, padx=self.padding)
+        back.grid(row=1, column=0, sticky='ew', padx=self.padding, pady=self.padding)
 
-        file_entry.bind("<1>", fetch_file)
-        file_label.grid(column=0, row=0, sticky='w', padx=padding)
-        file_entry.grid(column=1, row=0, columnspan=3, sticky='ew', padx=padding)
+        load_file.grid(row=1, column=1, columnspan=2, sticky='ew', padx=self.padding, pady=self.padding)
 
-        advanced.grid(column=0, row=1, columnspan=4, sticky="ew", padx=padding)
-        submit.grid(column=0, row=2, columnspan=4, sticky="ew", padx=padding)
+    def output_options(self):
+        self.destroy_widgets()
 
-    def settings(self, path, advanced_state):
-        print(path)
-        self.advanced = advanced_state
-        curate_by_headers = None
-        curate_into_monthly = None
-        curate_by_station = None  # if single station, off.
-        curate_by_date = None
+        output_label = Label(self.frame, text="Select Output File Type", font=("TkDefaultFont", 20))
+        graph_option = Checkbutton(self.frame, text="Graphs", onvalue=True, offvalue=False,
+                                   variable=self.choices['output_options']['png'])
+        csv_option = Checkbutton(self.frame, text="CSV file", onvalue=True, offvalue=False,
+                                 variable=self.choices['output_options']['csv'])
 
-        single_station = False
-        if advanced_state:
-            padding = self.root.winfo_width() * .005
-            if len(path) < 1:
-                print("Path: {0}".format(path))
-                error('Please select a proper file.')
-                return
+        curation = Label(self.frame, text="Curation Options", font=("TkDefaultFont", 14))
+        monthly = Checkbutton(self.frame, text="Curate data by monthly.", onvalue=True, offvalue=False,
+                              variable=self.choices['monthly'])
+        station = Checkbutton(self.frame, text="Create file for each station", onvalue=True, offvalue=False,
+                              variable=self.choices['separate_files'])
+        date = Checkbutton(self.frame, text="Curate by date range. (intensive)", onvalue=True, offvalue=False,
+                           variable=self.choices['date'])
+        back = Button(self.frame, text="Back", command=self.local_file)
+        curate = Button(self.frame, text="Next", command=self.curate_options)
+        curate.bind('<1>')
 
-            search = Search(path)
-            adjustment = self.adjustment()
-            row = adjustment[0]
+        output_label.grid(row=0, column=0, sticky='ew', pady=self.padding * 2, columnspan=3)
+        graph_option.grid(row=1, column=0, columnspan=2)
+        csv_option.grid(row=1, column=1, columnspan=2)
+        curation.grid(row=2, column=0, columnspan=5, pady=(self.padding * 2, 0))
 
-            monthly = Checkbutton(self.frame, text="Curate data by monthly.")
-            station = Checkbutton(self.frame, text="Create file for each station")
-            date = Checkbutton(self.frame, text="Curate by date range. (intensive)")
+        monthly.grid(row=3, column=0, sticky='w')
+        station.grid(row=3, column=1, sticky='w')
+        date.grid(row=3, column=2, sticky='w')
 
-            font = ("TkDefaultFont", 12)
-            curation = Label(self.frame, text="Curation Options", font=font)
+        back.grid(row=4, column=0, sticky='ew', pady=self.padding * 2, padx=self.padding)
+        curate.grid(row=4, column=1, sticky='ew', columnspan=2, pady=self.padding * 2, padx=self.padding)
 
-            curation.grid(column=0, row=row, sticky='ew', columnspan=5, pady=(padding*2, 0))
-            row += 1
+        # TO DO: we need to update NewSearch with Checkbutton selections.
 
-            monthly.grid(column=1, row=row, sticky='w')
-            station.grid(column=2, row=row, sticky='w')
-            date.grid(column=3, row=row, sticky='w')
-            row += 1
+    def curate_options(self):
+        self.destroy_widgets()
 
-            headers = Label(self.frame, text="Headers (to include in curation)", name='headers', font=font)
-            headers.grid(column=0, row=row, columnspan=5, sticky='ew', pady=(padding*2, 0))
-            row += 1
+        headers = Label(self.frame, text="Headers", name='headers', font=("TkDefaultFont", 14))
+        headers.grid(row=0, column=1, columnspan=4, sticky='ew')
 
-            choices = {}
-            column = 1
-            max_col = 3
-            # test = Frame(self.frame)
-            # test.grid(row=row, column=column)
+        column = 1
+        max_col = 4
+        row = 1
 
-            try:
-                for choice in search.input_headers:
-                    choices[choice] = BooleanVar(value=False)
-                    button = Checkbutton(self.frame, text=choice, onvalue=1, offvalue=0, variable=choices[choice])
-                    button.grid(row=row, column=column, sticky='w')
-                    button.columnconfigure(column, weight=1)
-                    column += 1
-                    if column > max_col:
-                        column = 1
-                        row += 1
+        try:
+            for choice in self.search.input_headers:
+                button = Checkbutton(self.frame, text=choice, onvalue=1, offvalue=0,
+                                     variable=self.choices['header_options'][choice])
 
-            except:
-                error("Issue loading headers... are you sure you selected the right file?")
-                for widget in self.frame.winfo_children():
-                    if 'row' in widget.grid_info() and not isinstance(widget, Button):
-                        if widget.grid_info()['row'] > 0:
-                            widget.destroy()
-                return
+                button.grid(row=row, column=column, sticky='w')
+                column += 1
 
-            row += 1
-            self.root.nametowidget('.main.file_entry').grid(column=1, columnspan=3)
-            self.root.nametowidget('.main.settings').configure(text='Disable Advanced Settings')
-            self.root.nametowidget('.main.settings').grid_configure(column=1, row=row, columnspan=3)
-            row += 1
-            self.root.nametowidget('.main.submit').grid_configure(column=1, row=row, columnspan=3)
-
-            for col_num in range(self.frame.grid_size()[0]):
-                self.frame.grid_columnconfigure(col_num, weight=1, uniform='fred')
-        else:
-
+                if column > max_col:
+                    column = 1
+                    row += 1
+        except Exception as err:
+            print(err)
+            error("Issue loading headers... are you sure you selected the right file?")
             for widget in self.frame.winfo_children():
                 if 'row' in widget.grid_info() and not isinstance(widget, Button):
                     if widget.grid_info()['row'] > 0:
                         widget.destroy()
-            self.root.nametowidget('.main.settings').configure(text='Advanced Settings')
+            return
 
+        back = Button(self.frame, text="Back", command=self.output_options)
+        submit = Button(self.frame, text="Submit", command=self.submit)
 
+        back.grid(row=4, column=1, sticky='ew', pady=self.padding * 2, padx=self.padding)
+        submit.grid(row=4, column=2, sticky='ew', columnspan=3, pady=self.padding * 2, padx=self.padding)
 
+    def submit(self):
+        output = {}
 
+        for key, val in self.choices.items():
 
-        choices = {}
+            if isinstance(val, dict):
+                for subkey, subval in val.items():
+                    if 'subkey' == 'DATE_OPT' and self.choices['date'].get():
+                        pass
+                    else:
+                        if subval.get():
+                            if key not in output:
+                                output[key] = [subkey]
+                            else:
+                                output[key].append(subkey)
+            else:
+                output[key] = val.get()
 
+        print(output)
 
-        """
-        max_col = 4
-        column=0
-
-        headers = Label(self.frame, text="Headers")
-        for choice in search.input_headers:
-            choices[choice] = BooleanVar(value=False)
-            button = Checkbutton(self.frame, text=choice, onvalue=1, offvalue=0, variable=choices[choice])
-            button.grid(row=row, column=column, sticky='w')
-            column += 1
-            if column > max_col:
-                column = 0
-                row += 1
-
-        if single_station:
-            pass
-
-        row += 1
-        for button in adjustment[1]:
-            button.grid(row=row, columnspan=5, sticky='ew')
-            row += 1
-         """
-
-    def adjustment(self):
-        row_higher = 0
-        removed = []
-        for item in self.frame.winfo_children():
-            if item.grid_info()['row'] > row_higher:
-                row_higher = item.grid_info()['row']
-        return [row_higher+1, removed]
-
-    def submit(self, path, options=None):
-        print("ee")
-        if not self.advanced:
-            pass
-            # options should be none
-        else:
-            pass
+    def destroy_widgets(self):
+        for widget in self.frame.winfo_children():
+            widget.destroy()
 
 
 def error(message):
@@ -202,36 +188,6 @@ def error(message):
     label.grid(column=0, row=0)
 
     root.mainloop()
-   
 
-"""
-
-file_label = Label(frame, text="File Path:")
-file_path = Entry(frame, state="readonly")
-
-
-def fetch_file():
-    file_path.delete(0, END)
-    f_path = filedialog.askopenfilename()
-    file_path.configure(state="normal")
-    file_path.insert(END, f_path)
-    file_path.configure(state="readonly")
-
-
-get_fi = ttk.Button(frame, text="Load file", command=fetch_file)
-submit = ttk.Button(frame, text="Submit", command=lambda: build(Path(file_path.get())))
-advanced = ttk.Button(frame, text="Advanced settings")
-
-
-frame.grid(column=0, row=0)
-file_label.grid(column=0, row=0)
-file_path.grid(column=1, row=0, columnspan=2)
-get_fi.grid(column=3, row=0)
-
-submit.grid(column=0, row=1, columnspan=4, sticky="ew")
-advanced.grid(column=0, row=2, columnspan=4, sticky="ew")
-file_path.icursor(20)
-
-"""
 
 s = Home()
--- search.py
+++ search.py
@@ -1,6 +1,51 @@
 import csv
 from pathlib import Path
+from enum import Enum
 
+
+class SearchType(Enum):
+    LOCAL_FILE = 1
+    NOAA_API = 2
+
+
+class Search:
+
+    def __init__(self, **kwargs):
+        self.search_type = None
+        self.local_file = None
+        self.path_file = None
+        self.curate_options = None
+        self.headers = None
+        self.output_options = None
+        self.input_headers = None
+
+        self.__dict__.update(kwargs)
+
+    def update(self, data):
+        for key, value in data.items():
+            if key == 'local_file':
+                setattr(self, 'path_file', Path(value))
+                self.input_headers = find_headers(self.path_file)
+            setattr(self, key, value)
+
+    def search(self):
+        if self.search_type == SearchType.LOCAL_FILE:
+            pass
+
+
+def find_headers(path_file):
+    with open(path_file, 'r') as input_file:
+        reader = csv.reader(input_file)
+        # fails on .xlsx files (need pandas :( )
+        for row in reader:
+            while len(row) > 0 and '' not in row[0:]:
+                return row
+
+    # indexes = [headers.index(i) for i in heads]
+    # this statement can be used to find the index of flagged headers that will be filtered. move upscope not downscope
+
+
+"""
 
 class Search:
 
@@ -14,11 +59,6 @@ class Search:
         if self.output_headers is not None:
             self.output_headers_id = [self.input_headers.index(i) for i in self.output_headers]
             self.output_headers_id.sort()
-
-
-def find_headers(file_path):
-    with open(file_path, 'r') as input_file:
-        reader = csv.reader(input_file)
-        for row in reader:
-            while len(row) > 0 and '' not in row[0:]:
-                return row
+            
+            
+"""
